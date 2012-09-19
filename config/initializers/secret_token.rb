4bb4a54378295f7aec4af7fbfe5a58e2c0762e22
Nathan Nontell <nathan.nontell@gmail.com>
2012-09-18 20:52:24 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Smithsongs::Application.config.secret_token = '5e8c6eee9f195ea19a5822356a4fc4f149f6999a465317e70ebc88117852168f96e685cc5c59f7ea1ae4e15ce73868b102587c2d18aba74b8abcf2ce8af8f919'
