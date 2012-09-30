3ed5681c22cb9609ed50a5f3bf22b6025cc55cef
NeutralAngel <nathan.nontell@gmail.com>
2012-09-30 00:33:39 -0400

    added jquery.color.js to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Smithsongs::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( jplayer.pink.flag.css jplayer.playlist.min.js jquery.jplayer.min.js mbScrollable.js )
+  config.assets.precompile += %w( jplayer.pink.flag.css jplayer.playlist.min.js jquery.jplayer.min.js mbScrollable.js jquery.color.js)
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
