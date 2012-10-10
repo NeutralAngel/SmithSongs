3937638ef8aa7c411293adf20399d716dbf72dd5
NeutralAngel <nathan.nontell@gmail.com>
2012-10-10 18:50:50 -0400

    Cart show/hide on add and delete

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -79,6 +79,7 @@ class CartsController < ApplicationController
 
     respond_to do |format|
       format.html { redirect_to store_url, notice: 'Your cart is now empty' }
+      format.js
       format.json { head :no_content }
     end
   end
