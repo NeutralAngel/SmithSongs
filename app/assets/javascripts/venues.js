d8df860d5afc4cdd4972c0c079b19613b77e9a71
NeutralAngel <nathan.nontell@gmail.com>
2012-11-03 18:12:35 -0400

    Venue pages now have maps

--- app/assets/javascripts/venues.js
+++ app/assets/javascripts/venues.js
@@ -3,5 +3,44 @@ jQuery(function($) {
   $('input.venue_submit').live("click", function() {
     history.pushState(null, "", "/venues/" + venue_id);
   });
+
+  initialize();
 });
+
+function initialize() {
+
+  var mapOptions = {
+    zoom: 12,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+
+  var address = document.getElementById("address").innerHTML;
+  var geocoder = new google.maps.Geocoder();
+  var map = new google.maps.Map(document.getElementById("map_canvas"),
+    mapOptions);
+
+  geocoder.geocode( { 'address': address}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        map.setCenter(results[0].geometry.location);
+        var marker = new google.maps.Marker({
+            map: map,
+            position: results[0].geometry.location
+        });
+      } else {
+        alert("Geocode was not successful for the following reason: " + status);
+      }
+    });
+
+  map = new google.maps.Map(document.getElementById("map_canvas"),
+    mapOptions);
+
+  };
+
+
+$(document).on("pjax:complete", function() {
+  return initialize();
+});
+    
+    
+ 
 
