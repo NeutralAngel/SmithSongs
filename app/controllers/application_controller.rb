07d79cc48d061b802df9fbc30ff29b533658f92b
Nathan Nontell <nathan.nontell@gmail.com>
2013-03-03 12:09:09 -0500

    Removed Carts for now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery
   helper_method :admin?
 
-  before_filter :set_music, :load_current_events, :load_cart
+  before_filter :set_music, :load_current_events
 
   def set_music
     @music_list = 
