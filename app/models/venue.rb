dea25d6c5c7a484e0cbaa637510be1b0d13fcd36
Nathan Nontell <nathan.nontell@gmail.com>
2013-09-04 21:18:42 -0400

    If photo url is empty, make nil

--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,4 +1,11 @@
 class Venue < ActiveRecord::Base
   attr_accessible :city, :line1, :line2, :name, :state, :zip, :photo_url
   has_many :events
+
+  before_save :make_photo_url_nil_if_empty
+
+	def make_photo_url_nil_if_empty
+	  self.photo_url = nil if self.photo_url.empty?
+	end
+
 end
